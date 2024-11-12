b7564cf046d5da0fc7280c6e3380f6c4b8fc1510
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-11-12 17:00:48 -0300

    Initial commit

---
+++ src/test/java/io/github/cursodsousa/produtoapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsousa.produtoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
